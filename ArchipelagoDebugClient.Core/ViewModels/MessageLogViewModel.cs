b7882721d4f79db43aba5388f9f27af08d71c0d5
Hussein Farran <hmfarran@gmail.com>
2026-02-14 22:47:27 -0500

    Add ability to send chat messages from log view.

--- ArchipelagoDebugClient.Core/ViewModels/MessageLogViewModel.cs
+++ ArchipelagoDebugClient.Core/ViewModels/MessageLogViewModel.cs
@@ -1,9 +1,11 @@
-﻿using Archipelago.MultiClient.Net;
+﻿using System.Collections.ObjectModel;
+using System.Reactive;
+using Archipelago.MultiClient.Net;
 using Archipelago.MultiClient.Net.MessageLog.Messages;
 using ArchipelagoDebugClient.Models;
 using ArchipelagoDebugClient.Services;
 using Avalonia.Threading;
-using System.Collections.ObjectModel;
+using ReactiveUI;
 
 namespace ArchipelagoDebugClient.ViewModels;
 
@@ -11,8 +13,19 @@ public class MessageLogViewModel : ViewModelBase
 {
     public ObservableCollection<BindableMessage> Messages { get; } = [];
 
+    private string _chatMessage = "";
+    public string ChatMessage
+    {
+        get => _chatMessage;
+        set => this.RaiseAndSetIfChanged(ref _chatMessage, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> SendMessageCommand { get; }
+
     public MessageLogViewModel(SessionProvider sessionProvider) : base(sessionProvider)
     {
+        SendMessageCommand = ReactiveCommand.Create(SendMessage, this.WhenAnyValue(x => x.HasSession, (bool hasSession) => hasSession));
+
         sessionProvider.OnSessionChanged += OnSessionChanged;
     }
 
@@ -31,5 +44,15 @@ public class MessageLogViewModel : ViewModelBase
     private void OnMessageRecieved(LogMessage message)
     {
         Dispatcher.UIThread.Invoke(() => Messages.Add(new BindableMessage(message)));
+    }
+
+    private void SendMessage()
+    {
+        string? message = string.IsNullOrEmpty(ChatMessage) ? null : ChatMessage;
+        if (message is not null)
+        {
+            Session!.Say(message);
+        }
+        ChatMessage = string.Empty;
     }
 }
